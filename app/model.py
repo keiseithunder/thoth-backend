48f19007149f69949b5024e6e61de5e0686d56fc
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-03-28 13:02:39 +0700

    dockernize app

--- app/model.py
+++ app/model.py
@@ -1,6 +1,5 @@
 import pickle
 
-
 class Model:
     __model = None
     __tfidfTransformer = None
@@ -17,9 +16,9 @@ class Model:
         if Model.__model != None and Model.__tfidfTransformer != None:
             raise Exception("Something went wrong") 
         else:
-            with open('text_classifier', 'rb') as spam_detection_model:
+            with open('spam_model/text_classifier', 'rb') as spam_detection_model:
                 Model.__model = pickle.load(spam_detection_model)
-            with open('TfidfTransformer', 'rb') as tfidfTransformer:
+            with open('spam_model/TfidfTransformer', 'rb') as tfidfTransformer:
                 Model.__tfidfTransformer = pickle.load(tfidfTransformer)
 
     @staticmethod
