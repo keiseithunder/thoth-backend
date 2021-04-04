29823e44477e0be4672f4709f9cb669f1c64d1ca
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2021-04-04 21:50:18 +0700

    update get feed

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Header, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import Model
 from tweepyHelper import TweepyHelper
 
-
 class Tweet(BaseModel):
     text: str
 
 class Response(BaseModel):
     class_label: str
+  
+class Status(BaseModel):
+    id: int
+    label: str
+    full_text: str
+
+class Feed(BaseModel):
+    feed: List[Status]
 
 app = FastAPI()
 
@@ -23,23 +30,25 @@ app.add_middleware(
 
 @app.get("/version")
 async def version():
-    return {"version": "0.0.2"}
+    return {"version": "0.0.3"}
 
 @app.post("/predict",response_model=Response)
 async def predict(tweet: Tweet):
-    if Model.getInstance() != None:
+
+    try :
       return {"class_label" : Model.predict([tweet.text])[0]}
-    else:
+    except:
       raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.get("/feed")
 async def feed(amount: int):
-    if TweepyHelper.getInstance() != None:
+    try:
+      TweepyHelper.getInstance()
       tweets = TweepyHelper.fetchTweet(amount)
       reslut = list(map(lambda x : x['full_text'],tweets))
-      print(reslut)
-      #print(predict_result)
-      #print(tweets[0].keys())
-      return {"feed":Model.predict(reslut)}
-    else:
+      prediction = Model.predict(reslut)
+      for index in range(len(tweets)):
+        tweets[index]['label'] = prediction[index]
+      return {"feed":tweets}
+    except:
       raise HTTPException(status_code=500, detail="Internal Server Error")
--- app/model.py
+++ app/model.py
@@ -1,28 +1,15 @@
 import pickle
 
 class Model:
-    __model = None
-    __tfidfTransformer = None
-
-    @staticmethod
-    def getInstance():
-        """ Static access method. """
-        if Model.__model == None and Model.__tfidfTransformer == None:
-            Model()
-        return Model.__model
-
-    def __init__(self):
-        """ Virtually private constructor. """
-        if Model.__model != None and Model.__tfidfTransformer != None:
-            raise Exception("Something went wrong") 
-        else:
-            with open('spam_model/text_classifier', 'rb') as spam_detection_model:
-                Model.__model = pickle.load(spam_detection_model)
-            with open('spam_model/TfidfTransformer', 'rb') as tfidfTransformer:
-                Model.__tfidfTransformer = pickle.load(tfidfTransformer)
 
     @staticmethod
     def predict(text):
-      transform = Model.__tfidfTransformer.transform(text)
-      return Model.__model.predict(transform)
+      tfidf = None
+      model = None
+      with open('spam_model/text_classifier', 'rb') as spam_detection_model:
+          model = pickle.load(spam_detection_model)
+      with open('spam_model/TfidfTransformer', 'rb') as tfidfTransformer:
+          tfidf = pickle.load(tfidfTransformer)
+      transform = tfidf.transform(text)
+      return model.predict(transform)
 
